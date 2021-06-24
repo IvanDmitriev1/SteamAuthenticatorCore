89ee96301d94d32e08b19f43063c193bd55ae7ac
Ivan <demaster24ru@icloud.com>
2021-06-24 16:30:40 +0600

    project reference fixed

--- Android/SteamAuthenticatorAndroid.Android/MainActivity.cs
+++ Android/SteamAuthenticatorAndroid.Android/MainActivity.cs
@@ -14,7 +14,7 @@ namespace SteamAuthenticatorAndroid.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            LoadApplication(new SteamAuthenticatorAndroid.App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
