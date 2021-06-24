b57992df8a204ddce3bbdb74d1bcc6a8aa4aeaa5
Ivan <demaster24ru@icloud.com>
2021-06-24 16:08:20 +0600

    Update MainActivity.cs

--- Android/SteamDesktopAuthenticatorCoreAndroid.Android/MainActivity.cs
+++ Android/SteamDesktopAuthenticatorCoreAndroid.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Runtime;
 
 namespace SteamAuthenticatorAndroid.Droid
 {
-    [Activity(Label = "SteamDesktopAuthenticatorCoreAndroid", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SteamAuthenticatorAndroid", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
