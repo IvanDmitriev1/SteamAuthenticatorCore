253e8823726cb562fe839bd656313010d446dddd
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2022-09-08 08:57:55 +0600

    deleted xamarin.forms project

--- src/SteamAuthenticatorCore.Mobile/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("MaterialIcons-Regular.ttf", Alias = "Material")] 
